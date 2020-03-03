7fd352e1b8bd592840c17812a628a3a119cc34a9
Roman Ianni <rmianni@gmail.com>
2020-03-03 10:15:03 -0300

    Modifico seteo local y borro archivos innecesarios

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,9 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const uri = process.env.ATLAS_URI;
+const uri = process.env.LOCAL_URI;
+
+console.log(uri);
 
 mongoose.connect(uri, {useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true});
 const connection = mongoose.connection;
