a4ae5b4a9ca43a149cbaf545155498431e465ba3
Roman <rmianni@gmail.com>
2020-03-02 23:09:37 -0300

    Added update, get and delete exercise

--- backend/routes/exercises.js
+++ backend/routes/exercises.js
@@ -26,4 +26,32 @@ router.route('/add').post( (req, res) => {
 
 });
 
+router.route('/:id').get((req, res) => {
+  Exercise.findById(req.params.id)
+    .then(exercise => res.json(exercise))
+    .catch(error => res.status(400).json('Error: ' + error))
+});
+
+router.route('/:id').delete((req, res) => {
+  Exercise.findByIdAndDelete(req.params.id)
+    .then(exercise => res.json('Exercise deleted!'))
+    .catch(error => res.status(400).json('Error: ' + error))
+});
+
+router.route('/update/:id').post((req, res) => {
+  Exercise.findById(req.params.id)
+    .then(exercise => {
+      console.log(req.params);
+      exercise.username = req.body.username;
+      exercise.description = req.body.description;
+      exercise.duration = req.body.duration;
+      exercise.date = Date.parse(req.body.date);
+     
+      exercise.save()
+        .then(exercise => res.json('Exercise updated!'))
+        .catch(error => res.status(400).json('Error: ' + error))
+      })
+    .catch(error => res.status(400).json('Error: ' + error))
+});
+
 module.exports = router;
